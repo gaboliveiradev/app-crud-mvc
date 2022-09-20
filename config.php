2adc0c3a456dd0738dde8d77060ba98a3a3622a1
gaboliveiradev <g38789138@gmail.com>
2022-09-20 08:55:08 -0300

    criando constantes de basedir e views e variáveis de ambiente

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+    /*
+     * BASEDIR: retorna o caminho/path do diretório pai, ou seja ele retorna todo o caminho
+     * de pastas desde o (C:). Irá pegar o diretório atual e subir levels acima a partir desde
+     * diretório.
+     */
+    DEFINE('BASEDIR', dirname(__FILE__, 2));
+    DEFINE('VIEWS', BASEDIR . "/View/modules");
+
+    /*
+     * Variáveis de Ambiente: as variáveis de ambiente são superglobal, ou seja ela está
+     * disponível em todo o escopo da aplicação.
+     */ 
+    $_ENV['db']['host'] = "localhost:3307";
+    $_ENV['db']['user'] = "root";
+    $_ENV['db']['pass'] = "mysqldev@2835";
+    $_ENV['db']['dbname'] = "php_mvc";
